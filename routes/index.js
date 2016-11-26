c8faacf309e9ac04c8e9a46c5b4162b537ca48d4
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-26 14:48:15 -0800

    adds user post method

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req, res, next) {
-  console.log('Something is happening...');
-  next();
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'myDeck'});
+  res.render('index', { title: 'myDeck' });
 });
 
 module.exports = router;
