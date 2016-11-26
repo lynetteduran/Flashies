51efc2b30a1af662d34d94d8ea29f869ad2c8e8b
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-26 14:45:36 -0800

    adds comments, removes unnecessary requirements, cleans up code

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-/*REQUIRE NODE MODULES*/
+/*SERVER CONFIG*/
+
+/**REQUIRE NODE MODULES**/
 var express        = require('express');
+var app            = express();
 var path           = require('path');
-var http           = require('http');
 var favicon        = require('serve-favicon');
 var cookieParser   = require('cookie-parser');
 var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var logger         = require('morgan');
-var errorHandler   = require('errorhandler');
 var static         = require('serve-static');
+var port           = process.env.PORT || 3000;
 
-var app    = express();
+/**DATABASE AND ALL ROUTES REQUIREMENTS/SET-UP**/
+var db     = require('./models/db');
+var user   = require('./models/user');
 var routes = require('./routes/index');
 var users  = require('./routes/users');
-
-/*SET-UP for ALL ENVIRONMENTS*/
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join( __dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-/*SET-UP for DEV ENVIRONMENT ONLY*/
-if('development' == app.get('env')){
-  app.use(errorHandler());
-}
+/**APPLICATION SET-UP**/
+app.set('views', path.join( __dirname, 'views'));
+app.set('view engine', 'jade');
 
-/*SET-UP SERVER PORT LISTENER*/
-http.createServer(app).listen(app.get('port'), function (){
-  console.log('Listening, I can hear you breathe... port ' + app.get('port'));
-});
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**SET-UP SERVER PORT LISTENER**/
+app.listen(port);
+console.log('I can hear you breathe...on port:' + port);
+
+/**EXPORTS EXPRESS APP SET-UP**/
+module.exports = app;
