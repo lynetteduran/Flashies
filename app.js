e6cefec7fcde6a604bd46b3652a31d11607c74f6
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-25 17:24:13 -0800

    changes view engine from ejs to jade, adds users index GET functionality

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-/*DATABASE*/
-var db = require('./models');
-
 /*REQUIRE NODE MODULES*/
-var express        = require( 'express' );
-var http           = require( 'http' );
-var auth           = require('basic-auth')
-var path           = require( 'path' );
-var engine         = require( 'ejs-locals' );
-// var favicon        = require( 'serve-favicon' );
-var cookieParser   = require( 'cookie-parser' );
-var bodyParser     = require( 'body-parser' );
-var methodOverride = require( 'method-override' );
-var logger         = require( 'morgan' );
-var errorHandler   = require( 'errorhandler' );
-var static         = require( 'serve-static' );
+var express        = require('express');
+var path           = require('path');
+var http           = require('http');
+var favicon        = require('serve-favicon');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var logger         = require('morgan');
+var errorHandler   = require('errorhandler');
+var static         = require('serve-static');
 
 var app    = express();
-var routes = require('./routes');
+var routes = require('./routes/index');
+var users  = require('./routes/users');
 
 /*SET-UP for ALL ENVIRONMENTS*/
-app.set( 'port', process.env.PORT || 3000 );
-app.engine( 'ejs', engine );
-app.set( 'views', path.join( __dirname, 'views' ));
-app.set( 'view engine', 'ejs' );
-// app.use( favicon( __dirname + '/public/favicon.ico' ));
-app.use( logger( 'dev' ));
-app.use( methodOverride());
-app.use( cookieParser());
-app.use( bodyParser.json());
-app.use( bodyParser.urlencoded({ extended : true }));
-app.use( static( path.join( __dirname, 'public' )));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join( __dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.post( '/users', routes.users.create );
-app.get('/', routes.users.index);
+app.use('/', routes);
+app.use('/users', users);
 
 /*SET-UP for DEV ENVIRONMENT ONLY*/
 if('development' == app.get('env')){
-  app.use( errorHandler());
+  app.use(errorHandler());
 }
 
 /*SET-UP SERVER PORT LISTENER*/
