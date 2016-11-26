6075a0f2fbcbc932257498670f128d3cce3c1378
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-26 14:49:00 -0800

    refactors homepage get method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  console.log('Something is happening...');
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'myDeck' });
+  res.render('index', {title: 'myDeck'});
 });
 
 module.exports = router;
