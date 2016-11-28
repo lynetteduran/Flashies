f90c02916f0143e7b36b98084252a83333bb7397
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-28 02:48:34 -0800

    deletes title from index page get method response

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use(function(req, res, next) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'myDeck'});
+  res.render('index');
 });
 
 module.exports = router;
